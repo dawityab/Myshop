e1a5ab06c12c1ec2e241e95cab826812e768be51
Dawit <dawityab@gmail.com>
2022-03-15 08:56:30 +0100

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetailComponent } from './detail/detail.component';
+import { CartComponent } from './cart/cart.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    MenuComponent,
+    DetailComponent,
+    CartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
